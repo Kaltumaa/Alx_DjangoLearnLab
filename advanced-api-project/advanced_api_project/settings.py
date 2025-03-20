1069de8bf80189fbbe1323c21bbe8a13aad9d67d
Kaltuma <kaltumaomar0@gmail.com>
2025-03-20 16:19:32 +0300

    Fixed directory naming: inner project directory renamed to advanced_api_project

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -31,14 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default Django apps…
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-'rest_framework',  # Django REST Framework
-    'api',]
+
+    # Third-party apps
+    'rest_framework',
+
+    # Your apps
+    'api',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
